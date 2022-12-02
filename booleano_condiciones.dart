ef848bbeb8dd3953249dee9673b4332b627d5732
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2022-12-02 15:46:56 -0500

    Add files that are Boolean-Conditions, Numbers-Strings

---
+++ booleano_condiciones.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+    //bool isActive = true;
+    bool? isActive = null;
+  
+    if( isActive == null) {
+      print("isActive es null");
+      //print("Estas activo");
+    } else {
+      print("no es null");
+      //print("Estas inactivo");
+    }
+    
+}
